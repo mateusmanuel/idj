8aa2512f73cae6b0af99657aef1eca812671d593
Mateus Manuel <mateusmanuel21@hotmail.com>
2018-04-04 20:43:47 -0300

    Implement GameObject class

---
+++ src/gameObject.cpp
@@ -0,0 +1,63 @@
+#include "gameObject.h"
+
+#include <algorithm>
+
+GameObject::GameObject()
+{
+    isDead = false;
+}
+
+GameObject::~GameObject()
+{
+    for(auto it: components)
+    {
+        delete(it);
+    }
+    components.clear();
+}
+
+void GameObject::Update(float dt)
+{
+    for(auto it: components)
+    {
+        it->Update(dt);
+    }
+}
+
+void GameObject::Render()
+{
+    for(auto it: components)
+    {
+        it->Render();
+    }
+}
+
+bool GameObject::IsDead()
+{
+    return isDead;
+}
+
+void GameObject::RequestDelete()
+{
+    isDead = true;
+}
+
+void GameObject::AddComponent(Component* cpt)
+{
+    components.push_back(cpt);
+}
+
+void GameObject::RemoveComponent(Component* cpt)
+{
+    components.erase(std::remove(components.begin(), components.end(), cpt), components.end());
+}
+
+Component* GameObject::GetComponent(std::string type)
+{
+    auto it = find_if(components.begin(), components.end(), [&type](Component* cpt) {return cpt->Is(type);});
+    
+    if (it == components.end())
+        return nullptr;
+
+    return *it;    
+}
